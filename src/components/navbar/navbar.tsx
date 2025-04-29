e046f886f7ee3e10ab279c24d8ead1d1cfd3cd52
guinafelix <oguilhermeleitefelix@gmail.com>
2025-04-29 08:46:04 -0300

    feat: configuracao inicial

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <img src="/profile.jpeg" alt="foto de perfil" />
+        <span>Guilherme Félix</span>
+      </div>
+      <div>
+        <span><a href="">HOME</a></span>
+        <span><a href="">SOBRE</a></span>
+        <span><a href="">PROJETOS</a></span>
+        <span><a href=""></a></span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
