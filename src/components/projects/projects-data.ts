291fe747a2b29dd10561fdf43145612ca17ffe04
guinafelix <oguilhermeleitefelix@gmail.com>
2025-05-05 19:04:13 -0300

    refactor: update project image filename

--- src/components/projects/projects-data.ts
+++ src/components/projects/projects-data.ts
@@ -11,7 +11,7 @@ export const ProjectsDB: ProjectsData[] = [
   {
     title: 'PlanejAI',
     description: "Esse projeto é uma ERP desenvolvida para gestão de microempresas consistindo na administração de fornecedores, clientes, produtos e também inclui um ChatBot interativo integrado com a API do ChatGPT. Foi desenvolido utilizando PHP7.4 e Laravel 7.",
-    imgUrl: '/planejai.svg',
+    imgUrl: '/planeja.svg',
     link: 'https://guinafelix.com.br/planejai',
     tools: ['php', 'laravel', 'eloquent', 'Integração ChatGPT API']
   }
